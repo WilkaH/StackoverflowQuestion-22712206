a1c02397e875f438631aeb9372da632faa9fc784
Wilka Hudson <wilka.hudson@nonlinear.com>
2014-03-28 13:38:08 +0000

    Added content

---
+++ LookslessVsUserControl/MrWhiteWithCodeBehind.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LookslessVsUserControl
+{
+    public partial class MrWhiteWithCodeBehind : UserControl
+    {
+        public static readonly DependencyProperty MyContentProperty = DependencyProperty.Register(
+            "MyContent", typeof (object), typeof (MrWhiteWithCodeBehind), new PropertyMetadata(default(object)));
+
+        public object MyContent
+        {
+            get { return (object)GetValue(MyContentProperty); }
+            set { SetValue(MyContentProperty, value); }
+        }
+
+
+        public MrWhiteWithCodeBehind()
+        {
+            InitializeComponent();
+        }
+    }
+}
